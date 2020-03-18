807c1d7825c5277496960dd3e5015a229e2b7a9d
Rob Spears <rob.sp3ars@gmail.com>
2020-03-18 14:22:58 -0400

    Add argument and function that shows location of ISS on map using turtle

--- iss.py
+++ iss.py
@@ -6,6 +6,7 @@ import requests
 import argparse
 import sys
 from datetime import datetime
+import turtle
 
 
 def get_astros():
@@ -55,6 +56,27 @@ def print_iss_loc(loc_data):
     print('\tTime:\t{}\n'.format(loc_data['time']))
 
 
+def show_iss_map(loc_data):
+    '''Shows a map with the location of the International
+     Space Station'''
+
+    # setup of background map and coordinates
+    world_map = turtle.Screen()
+    world_map.setup(width=720, height=360)
+    world_map.bgpic(picname='map.gif')
+    world_map.setworldcoordinates(-180, -90, 180, 90)
+
+    # add shape of Int'l Space Station
+    iss = 'iss.gif'
+    world_map.addshape(iss)
+    station = turtle.Turtle(shape=iss)
+    station.setheading(35.0)
+    station.penup()
+    station.goto((float(loc_data['lon']), float(loc_data['lat'])))
+
+    return world_map
+
+
 def create_parser():
     '''Creates and returns a command-line argument parser'''
     parser = argparse.ArgumentParser()
@@ -68,6 +90,11 @@ def create_parser():
                         dest='loc',
                         help='Show location data for the ' +
                         'International Space Station')
+    parser.add_argument('-m', '--map',
+                        action='store_true',
+                        dest='map',
+                        help='Show International Space Station ' +
+                        'location on a map')
     return parser
 
 
@@ -89,6 +116,10 @@ def main(args):
     if ns.loc:
         location = get_iss_location()
         print_iss_loc(location)
+    if ns.map:
+        location = get_iss_location()
+        world_map = show_iss_map(location)
+        world_map.exitonclick()
 
 
 if __name__ == '__main__':
