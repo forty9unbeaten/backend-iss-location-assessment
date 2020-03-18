94999dd9e5f72cb13fee6e6417217e4bbfbed9e8
Rob Spears <rob.sp3ars@gmail.com>
2020-03-18 12:49:54 -0400

    Add argument and define functions that get and print ISS location data

--- iss.py
+++ iss.py
@@ -4,6 +4,8 @@ __author__ = 'Rob Spears (GitHub: Forty9Unbeaten)'
 
 import requests
 import argparse
+import sys
+from datetime import datetime
 
 
 def get_astros():
@@ -18,7 +20,7 @@ def get_astros():
 
 
 def print_astro_info(astro_info):
-    '''Accepts JSON data about the astronauts currently in space
+    '''Accepts data about the astronauts currently in space
      and prints their name, the spacecrat they are on board and
      the number of astronauts in space'''
 
@@ -29,25 +31,65 @@ def print_astro_info(astro_info):
     print('\tTotal astronauts in space:\t{}\n'.format(len(astro_info)))
 
 
+def get_iss_location():
+    '''Make call to API and return location data for the
+     International Space Station'''
+
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    loc_info = r.json()
+    time = datetime.fromtimestamp(loc_info['timestamp'])
+    return {
+        'lat': loc_info['iss_position']['latitude'],
+        'lon': loc_info['iss_position']['longitude'],
+        'time': time.strftime('%B %d, %Y *** %I:%M:%S %p')
+    }
+
+
+def print_iss_loc(loc_data):
+    '''Accepts data about the location of the International
+     Space Station and prints the latitude, longitude and
+     the time'''
+    print('\n\tInternational Space Station Location:\n')
+    print('\tLatitude:\t{}'.format(loc_data['lat']))
+    print('\tLongitude:\t{}'.format(loc_data['lon']))
+    print('\tTime:\t{}\n'.format(loc_data['time']))
+
+
 def create_parser():
     '''Creates and returns a command-line argument parser'''
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--astronauts',
                         action='store_true',
                         dest='astros',
-                        help='Show a information about all ' +
+                        help='Show information about all ' +
                         'astronauts in space')
+    parser.add_argument('-l', '--location',
+                        action='store_true',
+                        dest='loc',
+                        help='Show location data for the ' +
+                        'International Space Station')
     return parser
 
 
-def main():
+def main(args):
+
+    # create argument parser and namespace of parsed
+    # shell arguments
     parser = create_parser()
     ns = parser.parse_args()
+
+    # if no arguments provided, show help prompt
+    if not args:
+        parser.print_help()
+        sys.exit()
 
     if ns.astros:
         astronauts = get_astros()
         print_astro_info(astronauts)
+    if ns.loc:
+        location = get_iss_location()
+        print_iss_loc(location)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
